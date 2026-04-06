00773e53a2ed2e610f7af1e029ede9b91cb9f395
Noah Carter <000918168@mohawkcollege.ca>
2026-04-06 13:56:41 -0400

    Footstep sounds

--- GarbageCleanup/Assets/Scripts/Footsteps.cs
+++ GarbageCleanup/Assets/Scripts/Footsteps.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    public AudioSource Grass_footsteps;
+    public AudioSource Grass_footsteps_new;
+    public float minPitch = 0.8f;
+    public float maxPitch = 1.1f;
+    float lastTime = 0;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
-            Grass_footsteps.enabled = true;
+            Grass_footsteps_new.enabled = true;
+            
         }
         else
         {
-            Grass_footsteps.enabled = false;
+            Grass_footsteps_new.enabled = false;
             
         }
+        if (Grass_footsteps_new.time < lastTime)
+        {
+            Grass_footsteps_new.pitch = Random.Range(minPitch, maxPitch);
+            Debug.Log("Pitch change");
+        } 
+        lastTime = Grass_footsteps_new.time;
+        
     }
 }
